6020f66f54a16a5f137a8a4a586e126d3afe713f
VanoKi <kirsvano@gmail.com>
2024-02-19 22:53:41 +0300

    feat: add currency api to the script.js

--- scripts/script.js
+++ scripts/script.js
@@ -43,34 +43,38 @@ document.getElementById('order-action').onclick = function () {
 
 }
 
-let prices = document.getElementsByClassName('products-item-price')
-document.getElementById('change-currency').onclick = function (e) {
-    let currentCurrency = e.target.innerText;
-    let newCurrency = '$';
-    let coefficient = 1;
-    if (currentCurrency === '$') {
-        newCurrency = '₽';
-        coefficient = 92;
-    } else if (currentCurrency === '₽'){
-        newCurrency = 'BYN';
-        coefficient = 3.236;
-    } else if (currentCurrency === 'BYN'){
-        newCurrency = '€';
-        coefficient = 0.93;
-    } else if (currentCurrency === '€'){
-        newCurrency = '¥';
-        coefficient = 7.19;
-    }
+function getCoefficient() {
+    fetch( 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json')
+        .then(response => response.json()) // Декодируем ответ в формате json
+        .then(data => {
+            let prices = document.getElementsByClassName('products-item-price')
+            document.getElementById('change-currency').onclick = function (e) {
+                let currentCurrency = e.target.innerText;
+                let newCurrency = '$';
+                let coefficient = 1;
+                if (currentCurrency === '$') {
+                    newCurrency = '₽';
+                    coefficient = data.usd['rub'];
+                } else if (currentCurrency === '₽'){
+                    newCurrency = 'BYN';
+                    coefficient = data.usd['byn'];
+                } else if (currentCurrency === 'BYN'){
+                    newCurrency = '€';
+                    coefficient = data.usd['eur'];
+                } else if (currentCurrency === '€'){
+                    newCurrency = '¥';
+                    coefficient = data.usd['cny'];
+                }
 
-    e.target.innerText = newCurrency;
-    for (let i = 0; i < prices.length; i++) {
-        prices[i].innerText = +(prices[i]
-            .getAttribute('data-base-price') * coefficient)
-            .toFixed(1) + ' ' + newCurrency;
-    }
+                e.target.innerText = newCurrency;
+                for (let i = 0; i < prices.length; i++) {
+                    prices[i].innerText = +(prices[i]
+                        .getAttribute('data-base-price') * coefficient)
+                        .toFixed(1) + ' ' + newCurrency;
+                }
+            }
+        });
 }
 
-// fetch( 'https://belarusbank.by/api/kursExchange')
-//     .then(response => response.json()) // Декодируем ответ в формате json
-//     .then(data => console.log(data));
+getCoefficient();
 

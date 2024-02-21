039b9669b30320387276961bb0a21d5cf31ba396
VanoKi <kirsvano@gmail.com>
2024-02-21 15:58:15 +0300

    feat: add sending dates to the server

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgForOf} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, NgForOf],
+  imports: [RouterOutlet, ReactiveFormsModule, NgForOf, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -119,19 +121,29 @@ export class AppComponent {
     },
   ]
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private appService: AppService) {
   }
+
   scrollTo(target: HTMLElement, burger?: any) {
-    target.scrollIntoView({behavior:'smooth'})
+    target.scrollIntoView({behavior: 'smooth'})
     if (burger) {
       this.form.patchValue({order: burger.title + ' (' + burger.price + ' ' + this.currency + ')'});
     }
   }
 
   confirmOrder() {
-    if (this.form.valid) {}
-    alert("Thanks for your order! We'll call you soon");
-    this.form.reset();
+    this.appService.sendOrder(this.form.value)
+      .subscribe(
+        {
+          next: (responce: any) => {
+            alert(responce.message);
+            this.form.reset();
+          },
+          error: (responce) => {
+            alert(responce.error.message)
+          },
+        }
+      )
   }
 
   changeCurrency() {
@@ -140,13 +152,13 @@ export class AppComponent {
     if (this.currency === '$') {
       newCurrency = '₽';
       coefficient = 80;
-    } else if (this.currency === '₽'){
+    } else if (this.currency === '₽') {
       newCurrency = 'BYN';
       coefficient = 3;
-    } else if (this.currency === 'BYN'){
+    } else if (this.currency === 'BYN') {
       newCurrency = '€';
       coefficient = 0.9;
-    } else if (this.currency === '€'){
+    } else if (this.currency === '€') {
       newCurrency = '¥';
       coefficient = 7;
     }
